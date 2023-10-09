8be0c695e9c8820de665f8190080a5cc6784aa00
acse-17b3c37e <ld823@ic.ac.uk>
2023-10-09 14:49:16 +0100

    add solve_matrix_equation

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 
-__all__ = ['rand_array']
+__all__ = ['rand_array', 'my_mat_solve']
 
 
 def rand_array(shape):
     return np.random.rand(*shape)
+
+
+def my_mat_solve(A, b):
+    return A.inv()*b
--- scripts/smooth_image.py
+++ scripts/smooth_image.py
@@ -1,5 +1,32 @@
 from envtest import rand_array
+from scipy.ndimage import gaussian_filter
+from scipy import misc
+from scipy import misc
+import matplotlib.pyplot as plt
 
 shape = (3, 3)
 
 print(rand_array(shape))
+
+
+
+
+__all__ = ['rand_array', 'smooth_image']
+
+def smooth_image(a, sigma=1):
+    return gaussian_filter(a, sigma=sigma)
+
+
+
+
+image = misc.ascent()
+sigma = 5
+
+smoothed_image = smooth_image(image, sigma)
+
+f = plt.figure()
+f.add_subplot(1, 2, 1)
+plt.imshow(image)
+f.add_subplot(1, 2, 2)
+plt.imshow(smoothed_image)
+plt.show(block=True)
